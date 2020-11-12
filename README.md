e6b31598fe86a2539b9b160fcc7089a7fa140f9a
h3dema <henriquemoura@hotmail.com>
2020-11-12 14:20:51 +0100

    first upload

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # pdf_reader
-Extract pages from PDF and read it outloud
+
+This project contains a python module that extracts pages from PDF and reads them aloud.
+
+
+## Dependencies
+
+```
+pip install -r requirements.py
+```
+
+
+## Program
+
+``
+usage: read_pdf.py [-h] [--show] [--metadata] [--play] [--mp3] [--no-mp3]
+                   [--language LANGUAGE]
+                   pdf_file
+
+PDF reader
+
+positional arguments:
+  pdf_file             PDF filename
+
+optional arguments:
+  -h, --help           show this help message and exit
+  --show               show each page as read
+  --metadata           show document metadata
+  --play
+  --mp3                save MP3
+  --no-mp3             save MP3
+  --language LANGUAGE  language of the PDF used by the PDF reader
+``
+
---
+++ read_pdf.py
@@ -0,0 +1,48 @@
+import os
+import argparse
+import fitz  # this is pymupdf
+# import PyPDF2  # deprecated
+import gtts
+from gtts import gTTS
+from pygame import mixer
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='PDF reader')
+    parser.add_argument('pdf_file', type=str, help="PDF filename")
+    parser.add_argument('--show', action="store_true", default=False, help="show each page as read")
+    parser.add_argument('--metadata', action="store_true", default=False, help="show document metadata")
+    parser.add_argument('--play', action="store_true", default=False)
+
+    parser.add_argument('--mp3', dest="save", action="store_true", help="save MP3")
+    parser.add_argument('--no-mp3', dest="save", action="store_false", help="save MP3")
+    parser.set_defaults(save=True)
+
+    parser.add_argument('--language', type=str, default="en", help="language of the PDF used by the PDF reader")
+
+    args = parser.parse_args()
+    assert args.language in gtts.lang.tts_langs().keys()
+
+    # creating an object
+    with fitz.open(args.pdf_file) as doc:
+        if args.metadata:
+            for k, v in doc.metadata.items():
+                if v is not None:
+                    print(f"{k}: {v}")
+        all_text = ""
+        for page in doc:
+            text = page.getText()
+            if args.show:
+                print(text)
+            all_text += text
+
+        if args.save:
+            output = os.path.basename(args.pdf_file)
+            output = output.replace('.pdf', '.mp3')
+            myobj = gTTS(text=all_text, lang=args.language, slow=False)
+            myobj.save(output)
+
+    if args.play:
+        mixer.init()
+        mixer.music.load(output)
+        mixer.music.play()
