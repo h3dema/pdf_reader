4563c79a397293979cbf665d08a6c7a0e6d0c4fa
h3dema <henriquemoura@hotmail.com>
2020-12-05 22:34:11 +0100

    updates

--- read_pdf.py
+++ read_pdf.py
@@ -1,13 +1,13 @@
-import re
-import time
 import os
+import re
+import sys
+import time
 import argparse
 import fitz  # this is pymupdf
 # import PyPDF2  # deprecated
 import gtts
 from gtts import gTTS
 import signal
-import sys
 
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'   # to hide pygame hello msg
 from pygame import mixer
@@ -27,10 +27,16 @@ def clean_line(s):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='PDF reader')
-    parser.add_argument('pdf_file', type=str, help="PDF filename")
+    parser.add_argument('input_file', type=str, help="PDF or txt filename")
     parser.add_argument('--show', action="store_true", default=False, help="show each page as read")
     parser.add_argument('--metadata', action="store_true", default=False, help="show document metadata")
     parser.add_argument('--play', action="store_true", default=False)
+
+    parser.add_argument('--text', action="store_true", help="save PDF as TXT")
+
+    parser.add_argument('--from-pdf', dest="source_pdf", action="store_true", help="convert from PDF")
+    parser.add_argument('--from-txt', dest="source_pdf", action="store_false", help="convert from TXT")
+    parser.set_defaults(source_pdf=True)
 
     parser.add_argument('--mp3', dest="save", action="store_true", help="save MP3 (default)")
     parser.add_argument('--no-mp3', dest="save", action="store_false", help="dont save MP3")
@@ -39,35 +45,55 @@ if __name__ == "__main__":
     parser.add_argument('--language', type=str, default="en", help="language of the PDF used by the PDF reader")
 
     args = parser.parse_args()
-    try:
-        langs = gtts.lang.tts_langs()
-    except (TypeError, RuntimeError):
-        # sometimes need a second call to get the result (bug !!!)
-        langs = gtts.lang.tts_langs()
-    assert args.language in langs.keys()
+    langs = None
+    # retries = 0
+    # while langs is not None or retries > 3:
+    #     try:
+    #         langs = gtts.lang.tts_langs()
+    #     except (TypeError, RuntimeError):
+    #         # sometimes need a second call to get the result (bug !!!)
+    #         langs = gtts.tts.tts_langs()
+    #     retries += 1
+    # assert args.language in langs.keys()
+    # if args.language not in langs.keys():
+    #     print("Unknown language")
+    #     sys.exit(0)
 
     # creating an object
-    with fitz.open(args.pdf_file) as doc:
-        if args.metadata:
-            for k, v in doc.metadata.items():
-                if v is not None:
-                    print(f"{k}: {v}")
-        all_text = ""
-        print("Start converting...")
-        for page in doc:
-            text = page.getText()
-            if args.show:
-                print(text)
-            all_text += clean_line(text)
+    all_text = ""
+    if args.source_pdf:
+        with fitz.open(args.input_file) as doc:
+            if args.metadata:
+                for k, v in doc.metadata.items():
+                    if v is not None:
+                        print(f"{k}: {v}")
+            print("Start converting...")
+            for page in doc:
+                text = page.getText()
+                if args.show:
+                    print(text)
+                all_text += clean_line(text)
+        # only save PDF as TXT
+        if args.text:
+            output = os.path.basename(args.input_file)
+            output = output.replace('.pdf', '.txt')
+            print("\tCreating TXT file")
+            with open(output, 'w', encoding="utf-8") as f:
+                f.write(all_text)
+    else:
+        print("Read txt file...")
+        with fitz.open(args.input_file) as doc:
+            for text in doc:
+                all_text += clean_line(text)
 
-        if args.save:
-            output = os.path.basename(args.pdf_file)
-            output = output.replace('.pdf', '.mp3')
-            print("\tCreating MP3 file")
-            myobj = gTTS(text=all_text, lang=args.language, slow=False)
-            print("\tSaving MP3 file")
-            myobj.save(output)
-        print("Conversion finished")
+    if args.save:
+        output = os.path.basename(args.input_file)
+        output = output.replace('.pdf', '.mp3')
+        print("\tCreating MP3 file")
+        myobj = gTTS(text=all_text, lang=args.language, slow=False)
+        print("\tSaving MP3 file")
+        myobj.save(output)
+    print("Conversion finished")
 
     if args.play:
         mixer.init()
